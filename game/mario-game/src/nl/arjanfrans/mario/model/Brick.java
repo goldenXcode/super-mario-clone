bfdb94abefa727f1e2d938228988f066d41a30e6
arjan.frans1993@gmail.com <arjan.frans1993@gmail.com@7db78d65-7eaf-bb80-a5cb-664580df784c>
2013-09-21 14:57:56 +0000

    mergend Brick and Bonus class

--- game/mario-game/src/nl/arjanfrans/mario/model/Brick.java
+++ game/mario-game/src/nl/arjanfrans/mario/model/Brick.java
@@ -3,6 +3,7 @@ package nl.arjanfrans.mario.model;
 import nl.arjanfrans.mario.audio.Audio;
 import nl.arjanfrans.mario.graphics.Tiles;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Interpolation;
@@ -14,15 +15,24 @@ public class Brick extends StaticActor {
 	private TextureRegion texture;
 	private TextureRegion empty_texture;
 	private TextureRegion shatter;
+	private float stateTime;
+	private static Animation bonus_animation;
 	private int hitcount = 0;
 	private int maxhits = 1;
-	private boolean destructable = true;
+	private boolean destructable;
 	private Array<Actor> items;
+	private boolean bonus; //Whether this is a bonus tile or not
 
-	public Brick(World world, float x, float y, String color) {
+	public Brick(World world, float x, float y, String color, boolean bonus, 
+			boolean destructable) {
 		super(world);
+		this.bonus = bonus;
+		this.destructable = destructable;
 		this.setOrigin(x, y);
 		this.setBounds(x, y, 16 * (1/16f), 16 * (1/16f));
+		bonus_animation = Tiles.getAnimation(0.15f, "bonus_block");
+		bonus_animation.setPlayMode(Animation.LOOP_PINGPONG);
+		empty_texture = Tiles.getTile("brick_empty");
 		items = new Array<Actor>();
 		if(color.equals("brown")) {
 			texture = Tiles.getTile("brick");
@@ -34,14 +44,27 @@ public class Brick extends StaticActor {
 		}
 	}
 	
-
+	@Override
+	public void act(float delta) {
+		stateTime += delta;
+		super.act(delta);
+	}
+	
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
 		if(destroyed) {
 			batch.draw(shatter, this.getX(), this.getY(), this.getWidth(), this.getHeight());
 		}
 		else {
-			batch.draw(texture, this.getX(), this.getY(), this.getWidth(), this.getHeight());
+			if(items.size < 1 && hitcount > 0) {
+				batch.draw(empty_texture, this.getX(), this.getY(), this.getWidth(), this.getHeight());
+			}
+			else if(bonus) {
+				batch.draw(bonus_animation.getKeyFrame(stateTime), this.getX(), this.getY(), this.getWidth(), this.getHeight());
+			}
+			else {
+				batch.draw(texture, this.getX(), this.getY(), this.getWidth(), this.getHeight());
+			}
 		}
 	}
 
@@ -51,7 +74,8 @@ public class Brick extends StaticActor {
 				((Mushroom) items.pop()).appear();
 			}
 		}
-		if(mario_level > 1) {	
+		if(mario_level > 1) {
+			hitcount++;
 			if(items.size < 1) {
 				if(destructable) {
 					destroyed = true;
